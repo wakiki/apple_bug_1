1f4d90b476c8af689977205ccf26b4c00b7b405a
Steve Leung <scyl88@gmail.com>
2014-08-22 15:06:39 +0100

    Initial Commit

---
+++ test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by Steve Leung on 22/08/2014.
+//  Copyright (c) 2014 Vocalix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test2
+//
+//  Created by Steve Leung on 22/08/2014.
+//  Copyright (c) 2014 Vocalix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
